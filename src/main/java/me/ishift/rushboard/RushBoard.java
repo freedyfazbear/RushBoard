2881e24084658418a6e51a06c591e72a5b8ebfdc
PolskiStevek <polskistevek@op.pl>
2020-01-19 16:18:35 +0100

    Initial commit

---
+++ src/main/java/me/ishift/rushboard/RushBoard.java
@@ -0,0 +1,13 @@
+package me.ishift.rushboard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RushBoard extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
